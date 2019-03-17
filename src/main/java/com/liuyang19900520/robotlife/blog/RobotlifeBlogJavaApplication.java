9e89abbbb7be6470ca23cf28ef2d0d459742bca2
liuyang19900520 <liuyang19900520@hotmail.com>
2019-03-17 18:14:26 +0900

    清楚github  秘钥。。。尴尬

--- src/main/java/com/liuyang19900520/robotlife/blog/RobotlifeBlogJavaApplication.java
+++ src/main/java/com/liuyang19900520/robotlife/blog/RobotlifeBlogJavaApplication.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class RobotlifeBlogJavaApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(RobotlifeBlogJavaApplication.class, args);
     }
 }
